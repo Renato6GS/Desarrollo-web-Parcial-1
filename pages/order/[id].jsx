7a880275e937e445b34871923928df4764adeee1
Renatogranados <renatogranadosgt@gmail.com>
2022-08-13 10:53:25 -0600

    feat: add localstorage

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -1,45 +1,57 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { MENU } from '../../constants/MENU';
+import Link from 'next/link';
 
 import styles from './styles.module.css';
 
 export default function Order({ results }) {
   const [id] = results;
-  console.log(id);
-  console.log(MENU[id]);
   const { title, price, image } = MENU[id - 1];
-  console.log(title);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const getCounts = localStorage.getItem('counts');
+    setCount(getCounts);
+  }, []);
+
+  const handleLocalStorage = () => {
+    const getCounts = localStorage.getItem('counts') || 0;
+    if (Number.isNaN(Number(getCounts))) {
+      setCount(1);
+      localStorage.setItem('counts', 1);
+      return;
+    }
+    const newCount = parseInt(getCounts) + 1;
+    localStorage.setItem('counts', newCount);
+    setCount(newCount);
+  };
 
   return (
     <>
       <Head>
         <title>Order {title}</title>
       </Head>
-      {/* <main className={styles.mainContainer}>
-        <h1>Platillo {title}</h1>
-        <img src={image} alt={title} />
-        <p>Precio: {price}</p>
-        <button>Ordenar</button>
-      </main> */}
       <main className={styles.mainContainer}>
+        <Link href={'/'}>
+          <a className={styles.regresar}>Regresar</a>
+        </Link>
         <header className={styles.mainHeader}>
           <div className={styles.leftHeader}>
             <h1 className={styles.title}>
               Platillo <span className={styles.city}>{title}</span>
             </h1>
             <p className={styles.description}>{price}</p>
-            <a className={styles.btnMenu} href='#'>
+            <button className={styles.btnMenu} onClick={handleLocalStorage}>
               Ordenar
-            </a>
+            </button>
           </div>
           <div className={styles.rightHeader}>
             <img className={styles.imgHeader} src={image} alt='Pizza main' />
           </div>
         </header>
-        <div>
-          <p className={styles.counter}>
-            Platillos ya ordenados: <span className={styles.quantity}>0</span>
-          </p>
+        <div className={styles.counter}>
+          Platillos ya ordenados: <span className={styles.quantity}>{count}</span>
         </div>
       </main>
     </>
